565aa15b30fa52de4b64c15598fdb480916d8c43
Hassan Yousefi <hexboy2011@gmail.com>
2020-06-26 22:26:58 +0430

    add simple test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Kaseb.io link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Kaseb.io/i);
   expect(linkElement).toBeInTheDocument();
 });

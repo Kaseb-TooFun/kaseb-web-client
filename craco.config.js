64cd1ed92ddd3f90def4017e62ef12f682e814d8
Hassan Yousefi <hexboy2011@gmail.com>
2020-07-23 23:23:36 +0430

    add dev server to prevent cors error & update eslint config

--- craco.config.js
+++ craco.config.js
@@ -7,8 +7,21 @@ module.exports = {
 			options: {
 				source: "tsconfig",
 				baseUrl: "./src",
-				tsConfigPath: "./tsconfig.extend.json"
+				tsConfigPath: "./tsconfig.extend.json",
 			},
 		},
 	],
+	eslint: {
+		enable: false,
+	},
+	devServer: {
+		proxy: {
+			"/api": {
+				target: "https://dev-api.mykaseb.ir",
+				secure: true,
+				changeOrigin: true,
+				logLevel: "info",
+			},
+		},
+	},
 };

df1b6c00fe4a1726d02919a0248f9d670cf31651
Hassan Yousefi <hexboy2011@gmail.com>
2020-07-27 10:44:29 +0430

    fix express serve config file

--- serve.js
+++ serve.js
@@ -1,37 +1,26 @@
-// ID=1 BASE_URL=https://hexboy.ir OLD_PUBLIC_URL="https://dev.mykaseb.ir" PUBLIC_URL="http://localhost:3000" PORT=3000 node serve.js
+// BASE_URL=https://hexboy.ir OLD_PUBLIC_URL="https://dev.mykaseb.ir" PUBLIC_URL="http://localhost:3000" PORT=3000 node serve.js
 
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
 
-try {
-	fs.renameSync(
-		path.join(__dirname, "build", "index.html"),
-		path.join(__dirname, "build", "index-org.html")
-	);
-} catch (error) {}
-
 let content = fs
-	.readFileSync(path.join(__dirname, "build", "index-org.html"))
+	.readFileSync(path.join(__dirname, "build", "index.html"))
 	.toString();
 
-const id = process.env.ID || 0;
 const baseUrl = process.env.BASE_URL || "https://dev-api.mykaseb.ir";
 const oldPublicUrl = process.env.OLD_PUBLIC_URL || "https://dev.mykaseb.ir";
 const newPublicUrl = process.env.PUBLIC_URL || "https://dev.mykaseb.ir";
-const fileName = "index-" + id + ".html";
 
 content = content
 	.replace(new RegExp(oldPublicUrl, "g"), newPublicUrl)
 	.replace("__BASE_URL__", baseUrl);
 
-fs.writeFileSync(path.join(__dirname, "build", fileName), content);
-
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", function (req, res) {
-	res.sendFile(path.join(__dirname, "build", fileName));
+	res.send(content);
 });
 
 app.listen(process.env.PORT || 3000);
